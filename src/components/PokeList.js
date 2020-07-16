ac4b530e09b3af7449f27111dcae9054b280257d
Ahmaderta Rizqi <ahmadderta.rizqi30@gmail.com>
2020-07-16 18:47:20 +0700

    alt attribute, for accessibility

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -3,7 +3,7 @@ import { setIdFromURL } from '../utils';
 
 const PokeList = (props) => {
    const { pokemons, clickDetail, isLoading, loadMorePokemon, search, pokemonCategory } = props;
-   console.log(pokemons);
+   // console.log(pokemons);
    
    return (
       <div className="row" style={{ paddingTop: '15rem' }}>
@@ -36,7 +36,7 @@ const PokeItem = ({ pokemon, clickDetail }) => {
       <div className="columns lg-3 sm-6">
          <div className="poke-item" onClick={() => clickDetail(setIdFromURL(pokemon.url || pokemon.pokemon.url))} >
             <div className="image">
-               <img src={`${imgURL + setIdFromURL(pokemon.url || pokemon.pokemon.url)}.png`} alt="" />
+               <img src={`${imgURL + setIdFromURL(pokemon.url || pokemon.pokemon.url)}.png`} alt={`${pokemon.name}`} />
             </div>
             <div className="content">
                <h2 className="name">{pokemon.name || pokemon.pokemon.name}</h2>
