e08503bd8b51fcd7651f23265d6314c58da9d9b9
Ahmaderta Rizqi <ahmadderta.rizqi30@gmail.com>
2020-08-18 09:27:07 +0700

    remove setIdPokemon

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,3 @@
-export const setIdPokemons = (pokemon) => {
-   pokemon.forEach((item, idx) => {
-      return item.id = idx + 1;
-   });
-}
-
 export const setIdFromURL = (url) => {
    return url.match(/\/(pokemon)\/(\d+)/)[2];
 }
